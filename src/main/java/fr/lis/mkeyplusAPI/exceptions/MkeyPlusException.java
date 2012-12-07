df7602db1a56f2b8838373d95e9dd790893301f0
Thomas Burguiere <thomas.burguiere@upmc.fr>
2012-12-07 17:35:46 +0100

    added MkeyPlusException.java

---
+++ src/main/java/fr/lis/mkeyplusAPI/exceptions/MkeyPlusException.java
@@ -0,0 +1,18 @@
+package fr.lis.mkeyplusAPI.exceptions;
+
+/**
+ * @author Thomas Burguiere
+ * @created 2012-12-07
+ */
+public class MkeyPlusException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1295330146278137937L;
+
+	public MkeyPlusException(String message) {
+		super(message);
+	}
+
+}
