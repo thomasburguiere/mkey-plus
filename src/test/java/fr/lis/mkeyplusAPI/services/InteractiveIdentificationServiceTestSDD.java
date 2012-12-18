51640a5565b35f38be7fba326b1358506657877c
Thomas Burguiere <thomas.burguiere@upmc.fr>
2012-12-18 16:13:16 +0100

    added test methods

--- src/test/java/fr/lis/mkeyplusAPI/services/InteractiveIdentificationServiceTestSDD.java
+++ src/test/java/fr/lis/mkeyplusAPI/services/InteractiveIdentificationServiceTestSDD.java
@@ -1,107 +1,180 @@
 package fr.lis.mkeyplusAPI.services;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import model.CategoricalDescriptor;
-import model.Description;
+import model.Dataset;
 import model.DescriptionElementState;
 import model.Descriptor;
+import model.DescriptorNode;
 import model.DescriptorTree;
 import model.Item;
+import model.QuantitativeDescriptor;
+import model.QuantitativeMeasure;
 import model.State;
 
 import org.apache.log4j.Logger;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import services.DescriptorManagementService;
-import services.DescriptorTreeManagementService;
-import services.ItemManagementService;
+import IO.parser.SDDSaxParser;
+
 import utils.Utils;
 
 public class InteractiveIdentificationServiceTestSDD {
 	public Logger logger = Logger.getRootLogger();
-	private static String login;
-	private static String dbName;
-	private static String password;
 
-	private static List<Item> itemsInKB;
-	private static List<Descriptor> descriptorsInKb;
-	private static DescriptorTree dependencyTreeInKB;
+	private static Dataset datasetInSDD;
+	private static List<Item> itemsInSDD;
+	private static List<Descriptor> descriptorsInSDD;
+	private static DescriptorTree dependencyTreeInSDD;
+	private static List<DescriptorTree> descriptorTreesInSDD;
+	private static String sddUrlString = "http://localhost:8080/miscFiles/Cichorieae-fullSDD.xml";
 
+	/**
+	 * initial method which parses the original SDD file
+	 * 
+	 * @throws Exception
+	 */
 	@BeforeClass
-	public static void init() throws Exception {
+	public static void parse() throws Exception {
 
 		// set test properties file
 		Utils.setBundleConf(ResourceBundle.getBundle("confTest"));
-		login = Utils.getBundleConfElement("hibernate.connection.database.username");
-		dbName = Utils.getBundleConfElement("hibernate.connection.database.dbName");
-		password = Utils.getBundleConfElement("hibernate.connection.database.password");
 
-		dependencyTreeInKB = DescriptorTreeManagementService.read(DescriptorTree.DEPENDENCY_TYPE, true,
-				dbName, login, password);
-		itemsInKB = ItemManagementService.readAll(true, true, dbName, login, password);
-		descriptorsInKb = DescriptorManagementService.readAll(true, dbName, login, password);
+		datasetInSDD = null;
+		URLConnection urlConnection = null;
+		InputStream httpStream = null;
+
+		// testing the sdd URL validity
+		URL sddFileUrl = new URL(sddUrlString);
+		// open URL (HTTP query)
+		urlConnection = sddFileUrl.openConnection();
+		// Open data stream to test the connection
+		httpStream = urlConnection.getInputStream();
+
+		// parsing the sdd to retrieve the dataset
+		datasetInSDD = new SDDSaxParser(sddFileUrl).getDataset();
+
+		itemsInSDD = datasetInSDD.getItems();
+		descriptorsInSDD = datasetInSDD.getDescriptors();
+		descriptorTreesInSDD = datasetInSDD.getDescriptorTrees();
+
 	}
 
-	// @Test
-	// public void calculateSingleDiscriminantPowerScore() throws Exception {
-	//
-	// Descriptor descriptor = descriptorsInKb.get(0);
-	// float score = 0;
-	// if (descriptor.isCategoricalType()) {
-	// score = InteractiveIdentificationService.categoricalDescriptorScore(
-	// (CategoricalDescriptor) descriptor, itemsInKB, dbName, login, password, true);
-	//
-	// logger.info("catDesc " + descriptor.getName() + " score: " + score);
-	//
-	// } else if (descriptor.isQuantitativeType()) {
-	//
-	// score = InteractiveIdentificationService.quantitativeDescriptorScore(
-	// (QuantitativeDescriptor) descriptor, itemsInKB, dbName, login, password);
-	// logger.info("numDesc " + descriptor.getName() + " score: " + score);
-	// }
-	//
-	// }
+	public void testParse() throws Exception {
+
+		URLConnection urlConnection = null;
+		InputStream httpStream = null;
+
+		// testing the sdd URL validity
+		URL sddFileUrl = new URL(sddUrlString);
+		// open URL (HTTP query)
+		urlConnection = sddFileUrl.openConnection();
+		// Open data stream to test the connection
+		httpStream = urlConnection.getInputStream();
+
+		// parsing the sdd to retrieve the dataset
+		datasetInSDD = new SDDSaxParser(sddFileUrl).getDataset();
+
+	}
 
 	@Test
-	public void testGetDescriptorScoreMapXperSort() throws Exception {
-		InteractiveIdentificationService.getDescriptorsScoreMap(descriptorsInKb, itemsInKB,
-				dependencyTreeInKB, InteractiveIdentificationService.SCORE_XPER, true);
-	}
+	public void testInitializeIds() throws Exception {
+		long descriptorCounter = 0;
+		long stateCounter = 0;
+		for (Descriptor dInSdd : descriptorsInSDD) {
+			dInSdd.setId(descriptorCounter);
+			descriptorCounter++;
+			if (dInSdd.isCategoricalType()) {
+				for (State s : ((CategoricalDescriptor) dInSdd).getStates()) {
+					s.setId(stateCounter);
+					stateCounter++;
+				}
+			}
+		}
 
-	// @Test
-	// public void testGetDescriptiveDataJaccard() throws Exception {
-	// InteractiveIdentificationService.getDescriptiveData(descriptorsInKb, itemsInKB, dbName, login,
-	// password, InteractiveIdentificationService.JACCARD);
-	// }
-	//
-	// @Test
-	// public void testGetDescriptiveDataSokalMichener() throws Exception {
-	// InteractiveIdentificationService.getDescriptiveData(descriptorsInKb, itemsInKB, dbName, login,
-	// password, InteractiveIdentificationService.SOKAL_MICHENER);
-	// }
+		long itemCounter = 0;
+		long measureCounter = 0;
+		long descriptionCounter = 0;
+		long descriptionElementStateCounter = 0;
+		for (Item itemInSDD : itemsInSDD) {
+			itemInSDD.setId(itemCounter);
+			itemCounter++;
+			itemInSDD.getDescription().setId(descriptionCounter);
+			descriptionCounter++;
+			for (DescriptionElementState des : itemInSDD.getDescription().getDescriptionElements().values()) {
+				des.setId(descriptionElementStateCounter);
+				descriptionElementStateCounter++;
+				if (des.getQuantitativeMeasure() != null) {
+					des.getQuantitativeMeasure().setId(measureCounter);
+					measureCounter++;
+				}
+			}
+		}
+
+		long descriptorNodeCounter = 0;
+		for (DescriptorTree tree : datasetInSDD.getDescriptorTrees())
+			for (DescriptorNode node : tree.getNodes()) {
+				node.setId(descriptorNodeCounter);
+				descriptorNodeCounter++;
+			}
+	}
 
 	@Test
-	public void testGetRemainingItems() throws Exception {
-		Description testDescription = new Description();
-		CategoricalDescriptor ringsOnTail = null;
-		for (Descriptor desc : descriptorsInKb)
-			if (desc.getName().toLowerCase().startsWith("rings on tail"))
-				ringsOnTail = (CategoricalDescriptor) desc;
-		DescriptionElementState ringsOnTailDescriptionElementState = new DescriptionElementState();
-
-		State selectedState = new State("present");
-		State unSelectedState = new State("absent");
-		selectedState.setCategoricalDescriptor(ringsOnTail);
-		unSelectedState.setCategoricalDescriptor(ringsOnTail);
-
-		ringsOnTailDescriptionElementState.addState(selectedState);
-		testDescription.addDescriptionElement(ringsOnTail, ringsOnTailDescriptionElementState);
-
-		InteractiveIdentificationService.getRemainingItems(testDescription, itemsInKB, dbName, login,
-				password);
-
+	public void testInitializeEmptyDescriptions() throws Exception {
+		for (Item itemInSDD : itemsInSDD) {
+			for (Descriptor descriptor : descriptorsInSDD) {
+				if (itemInSDD.getDescriptionElement(descriptor.getId()) == null) {
+					DescriptionElementState descriptionElementState = new DescriptionElementState();
+					if (descriptor.isQuantitativeType()) {
+						descriptionElementState.setQuantitativeMeasure(new QuantitativeMeasure());
+					}
+					itemInSDD.addDescriptionElement(descriptor, descriptionElementState);
+				}
+			}
+		}
 	}
+
+	@Test
+	public void testSelectOrInitializeDependencyTree() throws Exception {
+		dependencyTreeInSDD = null;
+		if (descriptorTreesInSDD.size() > 0) {
+			dependencyTreeInSDD = descriptorTreesInSDD.get(0);
+			for (int i = 1; i < descriptorTreesInSDD.size(); i++) {
+				DescriptorTree tree = descriptorTreesInSDD.get(i);
+				if (tree.getType().equalsIgnoreCase(DescriptorTree.DEPENDENCY_TYPE))
+					dependencyTreeInSDD = tree;
+			}
+		} else {
+			dependencyTreeInSDD = new DescriptorTree();
+			dependencyTreeInSDD.setType(DescriptorTree.DEPENDENCY_TYPE);
+			for (Descriptor descriptor : datasetInSDD.getDescriptors())
+				dependencyTreeInSDD.addNode(new DescriptorNode(descriptor));
+		}
+	}
+
+	@Test
+	public void testGetAllDescriptorScores() throws Exception {
+		for (Descriptor descriptor : descriptorsInSDD) {
+			if (descriptor.isCategoricalType())
+				InteractiveIdentificationService.categoricalDescriptorScore(
+						(CategoricalDescriptor) descriptor, itemsInSDD, dependencyTreeInSDD, 0);
+			else if (descriptor.isQuantitativeType())
+				InteractiveIdentificationService.quantitativeDescriptorScore(
+						(QuantitativeDescriptor) descriptor, itemsInSDD, dependencyTreeInSDD, 0);
+		}
+	}
+
+	@Test
+	public void testGetScoreMap() throws Exception {
+		InteractiveIdentificationService.getDescriptorsScoreMap(descriptorsInSDD, itemsInSDD,
+				dependencyTreeInSDD, 1, true);
+	}
+
 }
