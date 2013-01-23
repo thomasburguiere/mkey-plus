4e9ded1e724dcf75d526d0035b6e2c4d8394bbb6
Thomas Burguiere <thomas.burguiere@upmc.fr>
2013-01-23 12:28:21 +0100

    formatting

--- src/main/java/fr/lis/mkeyplusAPI/services/ThreadComputDescriptorsScoreMap.java
+++ src/main/java/fr/lis/mkeyplusAPI/services/ThreadComputDescriptorsScoreMap.java
@@ -9,19 +9,18 @@ import model.Descriptor;
 import model.DescriptorTree;
 import model.Item;
 
-
-
 public class ThreadComputDescriptorsScoreMap implements Callable<Object[]> {
-	
+
 	private Descriptor descriptor = null;
 	private float discriminantPower = -1;
-	private  List<Item> items = null;
+	private List<Item> items = null;
 	private DescriptorTree dependencyTree = null;
 	private boolean considerChildScores = true;
-	private int scoreMethod=-1;
+	private int scoreMethod = -1;
 	private DescriptionElementState[][] descriptionMatrix;
-	
-	public ThreadComputDescriptorsScoreMap(List<Item> items,DescriptorTree dependencyTree,int scoreMethod, boolean considerChildScores,Descriptor descriptor,DescriptionElementState[][] descriptionMatrix){
+
+	public ThreadComputDescriptorsScoreMap(List<Item> items, DescriptorTree dependencyTree, int scoreMethod,
+			boolean considerChildScores, Descriptor descriptor, DescriptionElementState[][] descriptionMatrix) {
 		this.descriptor = descriptor;
 		this.items = items;
 		this.dependencyTree = dependencyTree;
@@ -29,25 +28,23 @@ public class ThreadComputDescriptorsScoreMap implements Callable<Object[]> {
 		this.scoreMethod = scoreMethod;
 		this.descriptionMatrix = descriptionMatrix;
 	}
-	
-	public Object[] call(){
+
+	public Object[] call() {
 		Object[] output = new Object[2];
-		
-		if (descriptor.isCategoricalType()
-				&& ((CategoricalDescriptor) descriptor).getStates().size() <= 0){
+
+		if (descriptor.isCategoricalType() && ((CategoricalDescriptor) descriptor).getStates().size() <= 0) {
 			discriminantPower = 0;
 		}
-		
+
 		else {
-			discriminantPower = InteractiveIdentificationService.getDiscriminantPower(descriptor, items, 0, scoreMethod,
-					considerChildScores, dependencyTree,descriptionMatrix);
+			discriminantPower = InteractiveIdentificationService.getDiscriminantPower(descriptor, items, 0,
+					scoreMethod, considerChildScores, dependencyTree, descriptionMatrix);
 
 		}
 
-		
 		output[0] = descriptor;
 		output[1] = discriminantPower;
 		return output;
 	}
-	
+
 }
